0279a70ec4094bd38de1ef652489b168eb5cced3
Rahmannnnn <arifingatrahman@gmail.com>
2020-04-12 06:50:58 +0700

    Menambah Fungsi BerandaPembeli dan KategoriProduk

---
+++ GUI/ModulPembeli/BerandaPembeli.py
@@ -0,0 +1,9 @@
+from tkinter import *
+import DefVar
+from ModulPembeli import showProduct
+
+def berandaPembeli():
+    berandaframe = Frame(DefVar.root, bg=DefVar.white)
+    berandaframe.place(x=200, y=50, height=550, width=600)
+
+    showProduct.showProduct("Search","All")
---
+++ GUI/ModulPembeli/KategoriProduk.py
@@ -0,0 +1,35 @@
+from tkinter import *
+import DefVar
+from ModulPembeli import PagePembeli, showProduct
+
+def kategoriProduk():
+    frame = Frame(DefVar.root, bg=DefVar.white)
+    frame.place(x=200, y=50, height=550, width=600)
+
+    #Label : Cari Berdasarkan Kategori
+    Judul = Label(frame, text = "Cari berdasarkan Kategori : ", bg=DefVar.white, font="Helvetica 12 bold")
+    Judul.place(x=10, y=100)
+
+    #Button : Tuna Netra
+    netra = Button(frame, text="Tuna Netra", font=DefVar.font, activebackground=DefVar.white, activeforeground="#000000", fg=DefVar.white, bg=DefVar.redcolor, padx=100, pady=5, relief=FLAT, width=10, command=lambda:showProduct.showProduct("Kategori","Tuna Netra"))
+    netra.place(x=150, y=150, anchor=W)
+
+    #Button : Tuna Rungu
+    rungu = Button(frame, text="Tuna Rungu", font=DefVar.font, activebackground=DefVar.white, activeforeground="#000000", fg=DefVar.white, bg=DefVar.redcolor, padx=100, pady=5, relief=FLAT, width=10, command=lambda:showProduct.showProduct("Kategori","Tuna Rungu"))
+    rungu.place(x=150, y=200, anchor=W)
+
+    #Button : Tuna Wicara
+    wicara = Button(frame, text="Tuna Wicara", font=DefVar.font, activebackground=DefVar.white, activeforeground="#000000", fg=DefVar.white, bg=DefVar.redcolor, padx=100, pady=5, relief=FLAT, width=10, command=lambda:showProduct.showProduct("Kategori","Tuna Wicara"))
+    wicara.place(x=150, y=250, anchor=W)
+
+    #Button : Tuna Daksa
+    daksa = Button(frame, text="Tuna Daksa", font=DefVar.font, activebackground=DefVar.white, activeforeground="#000000", fg=DefVar.white, bg=DefVar.redcolor, padx=100, pady=5, relief=FLAT, width=10, command=lambda:showProduct.showProduct("Kategori","Tuna Daksa"))
+    daksa.place(x=150, y=300, anchor=W)
+
+    #Button : Tuna Grahita
+    grahita = Button(frame, text="Tuna Grahita", font=DefVar.font, activebackground=DefVar.white, activeforeground="#000000", fg=DefVar.white, bg=DefVar.redcolor, padx=100, pady=5, relief=FLAT, width=10, command=lambda:showProduct.showProduct("Kategori","Tuna Grahita"))
+    grahita.place(x=150, y=350, anchor=W)
+
+    #Button : Back
+    back = Button(frame, text="Back", font= "Helvetica 8", activebackground=DefVar.redcolor, fg=DefVar.background, bg=DefVar.white, relief=FLAT, command=lambda:PagePembeli.pagePembeli(frame))
+    back.place(x=10, y=20, anchor=W)
