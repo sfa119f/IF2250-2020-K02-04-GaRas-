3ac462a764acc2b4eb9568a74152cfe63daa5ad7
Rahmannnnn <arifingatrahman@gmail.com>
2020-04-12 06:56:25 +0700

    Memperbaiki PagePembeli

--- GUI/ModulPembeli/PagePembeli.py
+++ GUI/ModulPembeli/PagePembeli.py
@@ -1,7 +1,7 @@
 from tkinter import *
 import DefVar
 import PageLogin
-from ModulPembeli import showProduct
+from ModulPembeli import showProduct, KategoriProduk, BerandaPembeli
 
 def pagePembeli(frame):
     frame.destroy()
@@ -11,22 +11,21 @@ def pagePembeli(frame):
     searchframe = Frame(DefVar.root, bg=DefVar.redcolor)
     searchframe.place(x=200, y=0, height=50, width=600)
 
-    berandaframe = Frame(DefVar.root, bg=DefVar.white)
-    berandaframe.place(x=200, y=50, height=550, width=600)
-    showProduct.showProduct("All")
+    BerandaPembeli.berandaPembeli()
+
     #Search
     searchtxt = Button(searchframe, text="Search", font="Helvetica 8", fg=DefVar.text, bg=DefVar.white)
     searchtxt.place(x=510, y=25, anchor=W)
     search = Entry(searchframe, bd=1, bg=DefVar.white, relief=GROOVE, width=75)
     search.place(x=50, y=25, anchor=W)
 
-    username = Label(menu, text="Arif Rahman", font="Helvetica 12 bold", fg=DefVar.white, bg=DefVar.background)
+    username = Label(menu, text="Username", font="Helvetica 12 bold", fg=DefVar.white, bg=DefVar.background)
     username.place(x=50, y=200, anchor=W)
 
-    beranda = Button(menu, text="Beranda", font= DefVar.font, activebackground=DefVar.redcolor, activeforeground=DefVar.white, fg=DefVar.white, bg=DefVar.background, padx=15, pady=5, relief=FLAT, width=10)
+    beranda = Button(menu, text="Beranda", font= DefVar.font, activebackground=DefVar.redcolor, activeforeground=DefVar.white, fg=DefVar.white, bg=DefVar.background, padx=15, pady=5, relief=FLAT, width=10, command=BerandaPembeli.berandaPembeli)
     beranda.place(x=40, y=250, anchor=W)
 
-    profil = Button(menu, text="Profil", font= DefVar.font, activebackground=DefVar.redcolor, activeforeground=DefVar.white, fg=DefVar.white, bg=DefVar.background, padx=15, pady=5, relief=FLAT, width=10)
+    profil = Button(menu, text="Kategori", font= DefVar.font, activebackground=DefVar.redcolor, activeforeground=DefVar.white, fg=DefVar.white, bg=DefVar.background, padx=15, pady=5, relief=FLAT, width=10, command=KategoriProduk.kategoriProduk)
     profil.place(x=40, y=300, anchor=W)
 
     transaksi = Button(menu, text="Transaksi", font= DefVar.font,activebackground=DefVar.redcolor, activeforeground=DefVar.white, fg=DefVar.white, bg=DefVar.background, padx=15, pady=5, relief=FLAT, width=10)
