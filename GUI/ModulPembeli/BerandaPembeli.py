7eba101fffdc3d53be1933cfb54eb0831c6d3744
sfa119f <syarifuddinfa@gmail.com>
2020-04-12 16:12:13 +0700

    Buat perpindahan halaman dan hapus class scrollable

--- GUI/ModulPembeli/BerandaPembeli.py
+++ GUI/ModulPembeli/BerandaPembeli.py
@@ -6,4 +6,8 @@ def berandaPembeli():
     berandaframe = Frame(DefVar.root, bg=DefVar.white)
     berandaframe.place(x=200, y=50, height=550, width=600)
 
-    showProduct.showProduct("Search","All")
+    showProduct.showProduct(1,"Search","All")
+    #Contoh Data
+    #List = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T"]
+
+    
