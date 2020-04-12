5ed771fcaeb6aed3e9df417425693ca385496554
Rahmannnnn <arifingatrahman@gmail.com>
2020-04-12 15:27:09 +0700

    menambah DefVar, menampilkan foto profil pembeli

--- GUI/ModulPembeli/PagePembeli.py
+++ GUI/ModulPembeli/PagePembeli.py
@@ -1,6 +1,7 @@
 from tkinter import *
 import DefVar
 import PageLogin
+from PIL import ImageTk,Image
 from ModulPembeli import showProduct, KategoriProduk, BerandaPembeli
 
 def pagePembeli(frame):
@@ -13,20 +14,31 @@ def pagePembeli(frame):
 
     BerandaPembeli.berandaPembeli()
 
+    DefVar.username = "Katara"
+
     #Search
     searchtxt = Button(searchframe, text="Search", font="Helvetica 8", fg=DefVar.text, bg=DefVar.white)
     searchtxt.place(x=510, y=25, anchor=W)
     search = Entry(searchframe, bd=1, bg=DefVar.white, relief=GROOVE, width=75)
     search.place(x=50, y=25, anchor=W)
 
-    username = Label(menu, text="Username", font="Helvetica 12 bold", fg=DefVar.white, bg=DefVar.background)
-    username.place(x=50, y=200, anchor=W)
+    #Foto Profil
+    img = Image.open("../Foto/pengguna/katara.png")
+    img = img.resize((130,95), Image.ANTIALIAS)
+    img = ImageTk.PhotoImage(img)
+    panel = Label(menu, image=img)
+    panel.place(x=30,y=80)
+    panel.img=img
+
+    #Username
+    username = Label(menu, text=DefVar.username, font="Helvetica 12 bold", fg=DefVar.white, bg=DefVar.background)
+    username.place(x=70, y=200, anchor=W)
 
     beranda = Button(menu, text="Beranda", font= DefVar.font, activebackground=DefVar.redcolor, activeforeground=DefVar.white, fg=DefVar.white, bg=DefVar.background, padx=15, pady=5, relief=FLAT, width=10, command=BerandaPembeli.berandaPembeli)
     beranda.place(x=40, y=250, anchor=W)
 
-    profil = Button(menu, text="Kategori", font= DefVar.font, activebackground=DefVar.redcolor, activeforeground=DefVar.white, fg=DefVar.white, bg=DefVar.background, padx=15, pady=5, relief=FLAT, width=10, command=KategoriProduk.kategoriProduk)
-    profil.place(x=40, y=300, anchor=W)
+    kategori = Button(menu, text="Kategori", font= DefVar.font, activebackground=DefVar.redcolor, activeforeground=DefVar.white, fg=DefVar.white, bg=DefVar.background, padx=15, pady=5, relief=FLAT, width=10, command=KategoriProduk.kategoriProduk)
+    kategori.place(x=40, y=300, anchor=W)
 
     back = Button(menu, text="Back", font= "Helvetica 8", activebackground=DefVar.redcolor, activeforeground=DefVar.white, fg=DefVar.white, bg=DefVar.background, relief=FLAT, command=lambda:PageLogin.start(menu))
     back.place(x=10, y=20, anchor=W)
