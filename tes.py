55d2783d55f603bdcc249ba3e8e781246430dff8
iqbaln11 <13518074@std.stei.itb.ac.id>
2020-04-12 12:27:22 +0700

    Nambah membeli

--- tes.py
+++ tes.py
@@ -50,6 +50,120 @@ def UpdateSpek(a, b):
     except mysql.connector.Error as e:
         print("Gagal mengubah spek produk : {}".format(e))
 
+<<<<<<< Updated upstream
+=======
+def UpdateHarga(a, b):
+    try:
+        mydb = mysql.connector.connect(
+            host="127.0.0.1",
+            user="root",
+            passwd="XXX",
+            database="YYY"
+        )
+        myc = mydb.cursor()
+        sql1 = "Update produk set harga = %s where nama = %s"
+        val1 = (b, a)
+
+        myc.execute(sql1, val1)
+        mydb.commit()
+        print("Berhasil mengubah harga produk!")
+    except mysql.connector.Error as e:
+        print("Gagal mengubah harga produk : {}".format(e))
+
+def UpdateNama(a, b):
+    try:
+        mydb = mysql.connector.connect(
+            host="127.0.0.1",
+            user="root",
+            passwd="XXX",
+            database="YYY"
+        )
+        myc = mydb.cursor()
+        sql1 = "Update produk set nama = %s where nama = %s"
+        val1 = (b, a)
+
+        myc.execute(sql1, val1)
+        mydb.commit()
+        print("Berhasil mengubah nama produk!")
+    except mysql.connector.Error as e:
+        print("Gagal mengubah nama produk : {}".format(e))
+
+def UpdateBerat(a, b):
+    try:
+        mydb = mysql.connector.connect(
+            host="127.0.0.1",
+            user="root",
+            passwd="XXX",
+            database="YYY"
+        )
+        myc = mydb.cursor()
+        sql1 = "Update produk set berat = %s where nama = %s"
+        val1 = (b, a)
+
+        myc.execute(sql1, val1)
+        mydb.commit()
+        print("Berhasil mengubah berat produk!")
+    except mysql.connector.Error as e:
+        print("Gagal mengubah berat produk : {}".format(e))
+
+def Menjual(a, b, c, d, e, f, g):
+    try:
+        mydb = mysql.connector.connect(
+            host="127.0.0.1",
+            user="root",
+            passwd="XXX",
+            database="YYY"
+        )
+        myc = mydb.cursor()
+        sql = "Insert into produk values (null, %s, %s, %s, %s, %s, %s, %s)"
+        val = (a, b, c, d, e, f, g)
+
+        myc.execute(sql, val)
+        mydb.commit()
+        print("Berhasil menambah produk!")
+    except mysql.connector.Error as e:
+        print("Gagal menambah produk : {}".format(e))
+
+def Membeli(a, b, namapem, idkurir):
+    try:
+        mydb = mysql.connector.connect(
+            host="127.0.0.1",
+            user="root",
+            passwd="XXX",
+            database="YYY"
+        )
+        mycursor = mydb.cursor()
+        sql = "Select * from produk where nama = %s"
+        val = (a, )
+        mycursor.execute(sql, val)
+        result = mycursor.fetchall()
+        temp = result[0][3]
+        temp1 = result[0][0]
+        harprod = result[0][2]
+        berat = result[0][4]
+        temp = temp-b
+        sql = "Update produk set stok = %s where nama = %s"
+        val = (temp, a)
+        mycursor.execute(sql, val)
+        mydb.commit()
+        sql = "Select * from menjual where id = %s"
+        val = (temp1, )
+        result1 = mycursor.fetchall()
+        namapen = result1[0][0]
+        sql = "Select * from kurir where id = %s"
+        val = (idkurir, )
+        mycursor.execute(sql, val)
+        result2 = mycursor.fetchall()
+        harga = (result2[0][2]*berat) + harprod
+        sql = "Insert into transaksi values (null, %s, %s, %s, %s, %s, 'Diterima')"
+        val = (namapem, namapen, temp1, idkurir, harga)
+        mycursor.execute(sql, val)
+        mydb.commit()
+        print("Berhasil membeli produk!")
+    except mysql.connector.Error as e:
+        print("Gagal membeli produk : {}".format(e))
+
+>>>>>>> Stashed changes
 def SearchProduk(a):
     try:
         mydb = mysql.connector.connect(
