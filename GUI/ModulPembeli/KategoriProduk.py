87c197f2277d0640fa34302a09a1a47cf40903f6
Rahmannnnn <arifingatrahman@gmail.com>
2020-04-12 17:17:19 +0700

    Memperbaiki Kategori Produk

--- GUI/ModulPembeli/KategoriProduk.py
+++ GUI/ModulPembeli/KategoriProduk.py
@@ -11,23 +11,23 @@ def kategoriProduk():
     Judul.place(x=10, y=100)
 
     #Button : Tuna Netra
-    netra = Button(frame, text="Tuna Netra", font=DefVar.font, activebackground=DefVar.white, activeforeground="#000000", fg=DefVar.white, bg=DefVar.redcolor, padx=100, pady=5, relief=FLAT, width=10, command=lambda:showProduct.showProduct("Kategori","Tuna Netra"))
+    netra = Button(frame, text="Tuna Netra", font=DefVar.font, activebackground=DefVar.white, activeforeground="#000000", fg=DefVar.white, bg=DefVar.redcolor, padx=100, pady=5, relief=FLAT, width=10, command=lambda:showProduct.showProduct(1, "Kategori","Tuna Netra"))
     netra.place(x=150, y=150, anchor=W)
 
     #Button : Tuna Rungu
-    rungu = Button(frame, text="Tuna Rungu", font=DefVar.font, activebackground=DefVar.white, activeforeground="#000000", fg=DefVar.white, bg=DefVar.redcolor, padx=100, pady=5, relief=FLAT, width=10, command=lambda:showProduct.showProduct("Kategori","Tuna Rungu"))
+    rungu = Button(frame, text="Tuna Rungu", font=DefVar.font, activebackground=DefVar.white, activeforeground="#000000", fg=DefVar.white, bg=DefVar.redcolor, padx=100, pady=5, relief=FLAT, width=10, command=lambda:showProduct.showProduct(1, "Kategori","Tuna Rungu"))
     rungu.place(x=150, y=200, anchor=W)
 
     #Button : Tuna Wicara
-    wicara = Button(frame, text="Tuna Wicara", font=DefVar.font, activebackground=DefVar.white, activeforeground="#000000", fg=DefVar.white, bg=DefVar.redcolor, padx=100, pady=5, relief=FLAT, width=10, command=lambda:showProduct.showProduct("Kategori","Tuna Wicara"))
+    wicara = Button(frame, text="Tuna Wicara", font=DefVar.font, activebackground=DefVar.white, activeforeground="#000000", fg=DefVar.white, bg=DefVar.redcolor, padx=100, pady=5, relief=FLAT, width=10, command=lambda:showProduct.showProduct(1, "Kategori","Tuna Wicara"))
     wicara.place(x=150, y=250, anchor=W)
 
     #Button : Tuna Daksa
-    daksa = Button(frame, text="Tuna Daksa", font=DefVar.font, activebackground=DefVar.white, activeforeground="#000000", fg=DefVar.white, bg=DefVar.redcolor, padx=100, pady=5, relief=FLAT, width=10, command=lambda:showProduct.showProduct("Kategori","Tuna Daksa"))
+    daksa = Button(frame, text="Tuna Daksa", font=DefVar.font, activebackground=DefVar.white, activeforeground="#000000", fg=DefVar.white, bg=DefVar.redcolor, padx=100, pady=5, relief=FLAT, width=10, command=lambda:showProduct.showProduct(1, "Kategori","Tuna Daksa"))
     daksa.place(x=150, y=300, anchor=W)
 
     #Button : Tuna Grahita
-    grahita = Button(frame, text="Tuna Grahita", font=DefVar.font, activebackground=DefVar.white, activeforeground="#000000", fg=DefVar.white, bg=DefVar.redcolor, padx=100, pady=5, relief=FLAT, width=10, command=lambda:showProduct.showProduct("Kategori","Tuna Grahita"))
+    grahita = Button(frame, text="Tuna Grahita", font=DefVar.font, activebackground=DefVar.white, activeforeground="#000000", fg=DefVar.white, bg=DefVar.redcolor, padx=100, pady=5, relief=FLAT, width=10, command=lambda:showProduct.showProduct(1, "Kategori","Tuna Grahita"))
     grahita.place(x=150, y=350, anchor=W)
 
     #Button : Back
